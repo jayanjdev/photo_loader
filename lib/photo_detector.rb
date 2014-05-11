de8403427a37c9c51cf9ddb9a4b66d6a68aaa743
Jayan Jacob <jayandevjacob@gmail.com>
2014-05-11 16:35:57 -0700

    move the detection into a seperate class

---
+++ lib/photo_detector.rb
@@ -0,0 +1,20 @@
+require 'listen'
+
+class PhotoDetector
+  extend Forwardable
+
+  def initialize(dirs)
+    @black_hole_manager = BlackHoleManager.new(dirs)
+
+    @listener = Listen.to(@black_hole_manger.black_holes) do |modified, added, removed|
+      added.each do |file|
+        PhotoLoader.new(file, @black_hole_manager).load
+      end
+      removed.each do |file|
+        @black_hole_manager.remove(file)
+      end
+    end
+  end
+  def_delegators :@listener, :start
+end
+

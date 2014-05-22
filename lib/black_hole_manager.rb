993cd868964cbb12ac3d5e2bf7eaece60c9a7cf0
Jayan Jacob <jayandevjacob@gmail.com>
2014-05-21 23:13:21 -0700

    remove unnecessary methods

--- lib/black_hole_manager.rb
+++ lib/black_hole_manager.rb
@@ -1,20 +1,18 @@
-class BlackHoleManager
+module PhotoLoader
+  class BlackHoleManager
+    attr_reader :files, :black_holes
 
-  def initialize(dirs)
-    @dirs = dirs
-    @files = []
-  end
+    def initialize(black_holes)
+      @black_holes= black_holes #there a nothing but folder for now
+      @files = []
+    end
 
-  def black_holes
-    @dirs
-  end
+    def found(f)
+      @files << f
+    end
 
-  def found(f)
-    @files << f
-  end
-
-  def remove(f)
-    @files.delete(f)
+    def remove(f)
+      @files.delete(f)
+    end
   end
 end
-

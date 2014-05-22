76e3736de88920f94288f5b935e3a26830e47757
Jayan Jacob <jayandevjacob@gmail.com>
2014-05-21 23:12:53 -0700

    added logger

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'active_record'
 require 'yaml'
 require 'logger'
+require 'listen'
+require 'exifr'
 
 
 MIGRATION_DIR = 'db/migrate'
@@ -12,3 +14,9 @@ require_relative '../models/photo'
 require_relative '../lib/black_hole_manager'
 require_relative '../lib/photo_loader'
 require_relative '../lib/photo_detector'
+
+module PhotoLoader
+  def self.logger
+    @logger ||= Logger.new(File.open(LOG_DIR + '/photo_load_log.log' , 'a+'))
+  end
+end

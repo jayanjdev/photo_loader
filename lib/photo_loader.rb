8adc74b096ffb139192f617dc792ce42c541939a
Jayan Jacob <jayandevjacob@gmail.com>
2014-05-23 15:50:40 -0700

    Added spec for photo create

--- lib/photo_loader.rb
+++ lib/photo_loader.rb
@@ -2,8 +2,8 @@ module PhotoLoader
   class Loader
     attr_reader :file
 
-    def initialize(file, black_hole_manger)
-      @black_hole_manager = black_hole_manger
+    def initialize(file, black_hole_manager)
+      @black_hole_manager = black_hole_manager
       @file = file
       @black_hole_manager.found(file)
     end
@@ -12,16 +12,19 @@ module PhotoLoader
       begin
         ::PhotoLoader.logger.info("loadding ... file #{file}")
         photo_file = EXIFR::JPEG.new(file)
-        new_location = PhotoLoader::Archiver.new(file, photo_file.date_time_original).location
-        Photo.create(:location => new_location,
-                 :type => 'jpeg',
+        new_location = PhotoLoader::Archiver.new(file, photo_file.date_time_original || photo_file.date_time).location
+        ::PhotoLoader.logger.info("createing new record with #{new_location}")
+        photo = Photo.new(:location => new_location,
                  :click_date => photo_file.date_time_original)
+        photo.save
+        ::PhotoLoader.logger.info("created new record #{photo.inspect}")
+
       rescue Exception => e
         ::PhotoLoader.logger.error("Error loading file #{file}")
         ::PhotoLoader.logger.error(e.inspect)
       else
         ::PhotoLoader.logger.info("removing... file #{file}")
-        @black_hole_manger.remove(file)
+        @black_hole_manager.remove(file)
       end
     end
   end
--- spec/lib/photo_loader_spec.rb
+++ spec/lib/photo_loader_spec.rb
@@ -10,10 +10,12 @@ describe PhotoLoader::Loader do
   end
 
   describe "#load" do
-    it "builds a photo model" do
+    it "builds a photo record" do
       black_hole_manager = PhotoLoader::BlackHoleManager.new('black_hole')
-      photo_loader = PhotoLoader::Loader.new('f', black_hole_manager)
+      black_hole_manager.stub(:remove)
+      photo_loader = PhotoLoader::Loader.new(APP_ROOT + '/spec/fixtures/test.jpg', black_hole_manager)
       photo_loader.load
+      Pathname.new(Photo.first.location).basename.to_s.should eql('test.jpg')
     end
   end
 end

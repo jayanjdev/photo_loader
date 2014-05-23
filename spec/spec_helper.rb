f8f570fcb1aee334369ca4cf515837cd172091e8
Jayan Jacob <jayandevjacob@gmail.com>
2014-05-23 12:00:26 -0700

    Added db cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,22 @@
 APP_ENV = 'test'
+require 'database_cleaner'
 require_relative '../config/environment.rb'
+
 PhotoLoader::Application.init
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.logger = PhotoLoader.logger
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+    FileUtils.rm_rf("#{ARCHIVE_BASE}/*.*")
+  end
+end
